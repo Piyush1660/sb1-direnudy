c024cea5cb51459c3714de88998b4bce090e56b1
Piyush1660 <privatepiyush4@gmail.com>
2025-02-08 23:41:35 +0530

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { MessageSquare, Github, CheckCircle, AlertCircle, Clock } from 'lucide-react';
 import LandingPage from './pages/LandingPage';
 import WhitelistApplication from './pages/WhitelistApplication';
 import RulesPage from "./pages/RulesPage";
 
 function App() {
   return (
-    <Router>
+   
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/apply" element={<WhitelistApplication />} />
       <Route path="/rules" element={<RulesPage />} />
     </Routes>
-    </Router>
+   
   );
 }
 

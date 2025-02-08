4c689fa19918464172bb3234bd84cd6c9e4d2941
Piyush1660 <privatepiyush4@gmail.com>
2025-02-09 00:19:53 +0530

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import WhitelistApplication from './pages/WhitelistApplication';
 import RulesPage from "./pages/RulesPage";
 
 function App() {
   return (
-   
+    
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/apply" element={<WhitelistApplication />} />
       <Route path="/rules" element={<RulesPage />} />
     </Routes>
-   
   );
-}
+};
 
 export default App;

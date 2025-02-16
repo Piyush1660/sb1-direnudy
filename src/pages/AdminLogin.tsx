1c296ffb7222ab21e0f8a5da0acb309199a269b6
Piyush1660 <privatepiyush4@gmail.com>
2025-02-16 06:58:32 +0530

    Commit

--- src/pages/AdminLogin.tsx
+++ src/pages/AdminLogin.tsx
@@ -12,7 +12,7 @@ const AdminLogin: React.FC = () => {
     setErrorMsg("");
 
     try {
-      const response = await fetch("https://citytownrp.netlify.app/.netlify/functions/admin-login", {
+      const response = await fetch("/.netlify/functions/admin-login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",

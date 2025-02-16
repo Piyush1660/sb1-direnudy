d2546fea496a3847b9c0a5cac6eb1fb74eae727c
Piyush1660 <privatepiyush4@gmail.com>
2025-02-17 02:33:52 +0530

    Commit

--- netlify/notifications.ts
+++ netlify/notifications.ts
@@ -1,6 +1,9 @@
 exports.handler = async () => {
     return {
       statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json' // Add this to ensure JSON response
+      },
       body: JSON.stringify([
         { id: 1, message: "New user registered!", date: "2025-02-17" },
         { id: 2, message: "Form submission received.", date: "2025-02-16" }
